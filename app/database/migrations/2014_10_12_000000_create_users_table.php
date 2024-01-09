9cdf2da33b63a11f8884c38e9ea312afabdf3efd
DESKTOP-JQOPBM2\marci <marciosobral2015@gmail.com>
2024-01-09 16:40:17 -0300

    feat: add migrations

--- app/database/migrations/2014_10_12_000000_create_users_table.php
+++ app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('phone')->nullable();
+            $table->string('customer')->nullable();
             $table->timestamps();
         });
     }
---
+++ app/database/migrations/2024_01_09_193016_create_invoice_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoice', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_external');
+            $table->integer('user_id');
+            $table->string('type');
+            $table->string('amount');
+            $table->string('dueDate');
+            $table->string('keyPix')->nullable();
+            $table->string('codeBar')->nullable();
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoice');
+    }
+};
